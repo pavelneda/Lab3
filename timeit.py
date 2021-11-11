26c237eabeb873cb3a544becc595ca1b7d2230c1
pavelneda <90754825+pavelneda@users.noreply.github.com>
2021-11-11 13:17:12 +0200

    Create timeit.py

---
+++ timeit.py
@@ -0,0 +1,31 @@
+import os
+from timeit import timeit
+with open("newfile.txt", "a") as f:
+    for i in range(200000000000):
+        if os.path.getsize("newfile.txt")>=1024 * 1024 * 50:
+            break
+        f.write(str(i)+"\n")   
+t= """
+with open("newfile.txt") as f:
+    s=0
+    line=f.readlines()
+    for i in line:
+        if i.strip().isdigit():
+            s+=int(i.strip())
+"""
+print(timeit(t,number=10)/10) 
+
+t= """
+with open("newfile.txt") as f:
+    s=0
+    for line in f:
+        if line.strip().isdigit():
+            s+=int(line.strip())
+"""
+print(timeit(t,number=10)/10) 
+
+t="""
+with open("newfile.txt") as f:
+    s=sum((int(i.strip()) for i in f if i.strip().isdigit()))
+"""
+print(timeit(t,number=10)/10) 
